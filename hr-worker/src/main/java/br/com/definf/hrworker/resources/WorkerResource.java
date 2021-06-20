d4110a0c080857e2a5938ca99403ab19c48732d4
Raul Jr <raulsilveirajr@yahoo.com.br>
2021-06-20 01:51:09 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/br/com/definf/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/definf/hrworker/resources/WorkerResource.java
@@ -36,6 +36,16 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		/*
+		try {
+			Thread.sleep(3000l);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		
 		logger.info("PORT: " + env.getProperty("local.server.port"));
 
 		Worker worker = workerRepository.findById(id).get();
