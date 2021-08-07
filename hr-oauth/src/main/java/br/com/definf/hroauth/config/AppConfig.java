b30dca47f302f91917ed48eca4748cb00c85c815
Raul Jr <raulsilveirajr@yahoo.com.br>
2021-08-07 18:34:06 -0300

    fix: @Bean missing

--- hr-oauth/src/main/java/br/com/definf/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/br/com/definf/hroauth/config/AppConfig.java
@@ -2,9 +2,6 @@ package br.com.definf.hroauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -24,7 +21,7 @@ public class AppConfig {
 		
 		return tokenConverter;
 	}
-	
+
 	@Bean
 	public JwtTokenStore tokenStore( ) {
 		return new JwtTokenStore(accessTokenConverter());
